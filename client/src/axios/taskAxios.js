8323b34b9523078ccfafd72ba65c5ccd2f94d317
Angel Neupane <angelneupane83@gmail.com>
2025-02-23 10:15:30 +1100

    FE build

--- client/src/axios/taskAxios.js
+++ client/src/axios/taskAxios.js
@@ -1,7 +1,7 @@
 // This file sends api request for task resource
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_BASE_API_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const TASK_ENDPOINT = "/api/tasks";
 
 // FRONTEND CRUD OPERATION
